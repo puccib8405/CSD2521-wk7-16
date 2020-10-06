00ba86c674eda061d5a96f1bbfa6a1fdd71367d8
puccib8405 <70244468+puccib8405@users.noreply.github.com>
2020-10-06 17:41:28 -0400

    Update HelloTest1.java
    I added an update on line 3 and modified the comments on line 8

--- HelloTest1.java
+++ HelloTest1.java
@@ -1,11 +1,12 @@
 /*
  * Created on 8/3/18 by AJT
+ * Updated on 10/6/2020 by BRP
  */
 package hello.test.pkg1;
 
 public class HelloTest1 {
     /*
-     * Replace your comments here!!
+     * This is my first pull request
      */
     public static void main(String[] args) {
         System.out.println("Welcome");
